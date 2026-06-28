b83ab73d14c4439cf3e477f6f013cf2bf03e68ea
Tiep Pham <payvip1802@gmail.com>
2026-06-28 17:38:27 +0700

    Revert "fix: serve Vietnamese localized storefront routes"
    This reverts commit b8a0ea06f3479aa5cc087d5372feed918b9558f0.

--- src/app/[locale]/bai-viet/[postSlug]/page.tsx
+++
@@ -1,4 +0,0 @@
-export const dynamic = 'force-static';
-export const revalidate = 300;
-
-export { default, generateMetadata } from '../../blog/[postSlug]/page';
--- src/app/[locale]/bai-viet/page.tsx
+++
@@ -1,4 +0,0 @@
-export const dynamic = 'force-static';
-export const revalidate = 300;
-
-export { default } from '../blog/page';
